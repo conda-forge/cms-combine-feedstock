9a15bcc565e7c6553d7cfd9e5a811a0a5d115660
Nick Smith <nick.smith@cern.ch>
2025-02-27 17:49:07 -0600

    Try hinting

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-python3 -c "import sysconfig; print(sysconfig.get_path('platlib'))"
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX -S HiggsAnalysis/CombinedLimit -B build
+cmake ${CMAKE_ARGS} -DPython_ROOT_DIR=$PREFIX -S HiggsAnalysis/CombinedLimit -B build
 cmake -LH build
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
 cmake --install build
