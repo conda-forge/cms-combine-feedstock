8dd2c522e28cb6f95927075956117455c0a4005b
Nick Smith <nick.smith@cern.ch>
2025-02-27 08:01:49 -0600

    Suggested build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ex
 
-pushd HiggsAnalysis/CombinedLimit
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX ..
-make -j${CPU_COUNT}
-make install
-popd
+cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX -S HiggsAnalysis/CombinedLimit -B build
+cmake -LH build
+cmake --build build --clean-first --parallel="${CPU_COUNT}"
+cmake --install build
