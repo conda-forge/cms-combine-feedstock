10bdcfe3c755fe911a4d28df0cb88e4e63667f9b
Nick Smith <nick.smith@cern.ch>
2025-02-26 22:30:44 -0600

    Try existing cmake recipe?

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,9 @@
 set -ex
 
 pushd HiggsAnalysis/CombinedLimit
-make CONDA=1 -j${CPU_COUNT}
-cp -rv build/bin ${PREFIX}
-cp -v build/lib/*${SHLIB_EXT} ${PREFIX}/lib
-cp -v build/lib/*.pcm ${PREFIX}/lib
-cp -v build/lib/*.rootmap ${PREFIX}/lib
-mkdir -p ${PREFIX}/include/HiggsAnalysis/CombinedLimit
-cp -rv interface ${PREFIX}/include/HiggsAnalysis/CombinedLimit/
-cp -rv build/lib/python/HiggsAnalysis $(python3 -c "import sysconfig; print(sysconfig.get_path('platlib'))")/
+mkdir build
+cd build
+cmake ..
+make -j${CPU_COUNT}
+make install
 popd
