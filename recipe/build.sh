367bf07860a05ea5e9304152673ee0a799cb5acc
Nick Smith <nick.smith@cern.ch>
2025-02-28 10:14:33 -0600

    Hopefully platlib is patched?

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-cmake ${CMAKE_ARGS} -DPython_SITELIB=$(python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))") -S HiggsAnalysis/CombinedLimit -B build
+python3 -m sysconfig
+cmake ${CMAKE_ARGS} -DPython_SITELIB=$(python3 -c "import sysconfig; print(sysconfig.get_path('platlib'))") -S HiggsAnalysis/CombinedLimit -B build
 cmake -LH build
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
 cmake --install build
