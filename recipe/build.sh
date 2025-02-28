0ad385a621c78a2897031fed6911aae82639cccb
Nick Smith <nick.smith@cern.ch>
2025-02-28 16:14:06 -0600

    Try it out

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,9 @@ set -ex
 # But at least they will have the same version
 Python_SITELIB=$PREFIX$(python3 -c "import sysconfig;p=sysconfig.get_path('purelib');print(p[p.rfind('/lib'):])")
 cmake ${CMAKE_ARGS} -DPython_SITELIB=$Python_SITELIB -S HiggsAnalysis/CombinedLimit -B build
-grep "rootcling" build/CMakeFiles/CMSCombine.dir/build.make
+if [[ "${target_platform}" == "osx-arm64" && "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+  echo "Hack to use rootcling from BUILD_PREFIX"
+  sed -i .bak "s#/bin/cmake -E env .*/bin/rootcling#/bin/rootcling#" build/CMakeFiles/CMSCombine.dir/build.make
+fi
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
 cmake --install build
