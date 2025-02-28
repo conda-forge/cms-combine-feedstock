b0f79f29e115b0f10e0dc945f7a2387bfbde6ed9
Nick Smith <nick.smith@cern.ch>
2025-02-28 08:56:12 -0600

    more debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+realpath $PREFIX
+ls $PREFIX/bin
+ls $PREFIX/lib/python*
 cmake ${CMAKE_ARGS} -DPython_ROOT_DIR=$PREFIX -S HiggsAnalysis/CombinedLimit -B build
 cmake -LH build
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
