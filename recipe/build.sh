b843e183594dc091b0b825e67aac2d6e3810e93f
Nick Smith <nick.smith@cern.ch>
2025-02-28 09:42:54 -0600

    packaging is suffering
    You try to do things right and get punished
    You do the hack and move on with your life

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-cmake ${CMAKE_ARGS} -DPython_ROOT_DIR=$PREFIX -S HiggsAnalysis/CombinedLimit -B build
+cmake ${CMAKE_ARGS} -DPython_SITELIB=$(python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))") -S HiggsAnalysis/CombinedLimit -B build
 cmake -LH build
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
 cmake --install build
