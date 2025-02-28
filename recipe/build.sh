3949656cd43adda622a3b336fd125c7ac82d147a
Nick Smith <nick.smith@cern.ch>
2025-02-28 11:38:01 -0600

    investigate cxx flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ex
 
-python3 -m sysconfig
-cmake ${CMAKE_ARGS} -DPython_SITELIB=$(python3 -c "import sysconfig; print(sysconfig.get_path('platlib'))") -S HiggsAnalysis/CombinedLimit -B build
-cmake -LH build
+# When cross-compiling, we cannot get the host python sitelib, only the build one
+# But at least they will have the same version
+Python_SITELIB=$PREFIX$(python3 -c "import sysconfig;p=sysconfig.get_path('purelib');print(p[p.rfind('/lib'):])")
+cmake ${CMAKE_ARGS} -DPython_SITELIB=$Python_SITELIB -S HiggsAnalysis/CombinedLimit -B build
+cmake -LAH build
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
 cmake --install build
