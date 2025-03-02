6752b3d09803624078c6c58c22a3598e7801a203
Nick Smith <nick.smith@cern.ch>
2025-03-02 10:09:09 -0600

    Fix also the cross-compile hack

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ Python_SITELIB=$PREFIX$(python3 -c "import sysconfig;p=sysconfig.get_path('purel
 cmake ${CMAKE_ARGS} -DPython_SITELIB=$Python_SITELIB -S HiggsAnalysis/CombinedLimit -B build
 if [[ "${target_platform}" == "osx-arm64" && "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   echo "Hack to use rootcling from BUILD_PREFIX"
-  sed -i "s#/bin/cmake -E env .*/bin/rootcling#/bin/rootcling#" build/CMakeFiles/CMSCombine.dir/build.make
+  sed -i "s#/bin/cmake -E env .*/bin/rootcling#/bin/rootcling#" build/CMakeFiles/HiggsAnalysisCombinedLimit.dir/build.make
 fi
 cmake --build build --clean-first --parallel="${CPU_COUNT}"
 cmake --install build
