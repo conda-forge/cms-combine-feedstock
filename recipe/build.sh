46be9da915f5e7d5b21bbb10b7caef3a2084b503
andrzejnovak <novak5andrzej@gmail.com>
2025-04-02 08:26:10 +0200

    patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ex
 
+if [[ "${target_platform}" == "linux_aarch64" ]]; then
+  export CFLAGS="${CFLAGS//-march=nocona/} ${CFLAGS//-mtune=haswell/} -march=armv8-a"
+  export CXXFLAGS="${CXXFLAGS//-march=nocona/} ${CXXFLAGS//-mtune=haswell/} -march=armv8-a"
+  export LDFLAGS="${LDFLAGS}"
+fi
+
 # When cross-compiling, we cannot get the host python sitelib, only the build one
 # But at least they will have the same version
 Python_SITELIB=$PREFIX$(python3 -c "import sysconfig;p=sysconfig.get_path('purelib');print(p[p.rfind('/lib'):])")
